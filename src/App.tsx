95cc918abace53aca28c354b67707f4b91886642
hasaigal <hasaigal@gmail.com>
2024-04-26 22:49:38 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ interface Todo {
 function App() {
   const [todo, setTodo] = useState<Todo[]>([]);
   const [userInput, setUserInput] = useState("");
+  const [isEditable, setIsEditable] = useState(true);
 
   // adding todo state
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -38,8 +39,23 @@ function App() {
     );
   };
 
+  const handleEdit = (todoText: string, id: string) => {
+    setTodo((todo) =>
+      todo.map((todo) => {
+        if (id === todo.id) {
+          return {
+            ...todo,
+            todo: todoText,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   const handleDelete = (id: string) => {
-    setTodo((todo) => todo.filter((todo) => todo.id!== id))
+    setTodo((todo) => todo.filter((todo) => todo.id !== id));
   };
 
   return (
@@ -56,29 +72,47 @@ function App() {
         <button type="submit">submit</button>
       </form>
 
-      <ul style={{ width: "300px", paddingLeft: "20px" }}>
+      <div style={{ width: "300px", paddingTop: "20px" }}>
         {todo.map((item) => (
-          <li
+          <div
             key={item.id}
             style={item.completed ? { textDecoration: "line-through" } : {}}
           >
             <input type="checkbox" onClick={() => handleChecked(item.id)} />
-            {item.todo}
+            <input
+              type="text"
+              readOnly={isEditable}
+              value={item.todo}
+              style={item.completed ? { textDecoration: "line-through" } : {}}
+              onChange={(e) => handleEdit(e.target.value, item.id)}
+            />
 
             <span
               style={{
                 color: "red",
                 display: "inline-block",
-                paddingLeft: "100px",
+                // paddingLeft: "100px",
+                cursor: "pointer",
+              }}
+              onClick={() => setIsEditable(!isEditable)}
+            >
+              &#9998;
+            </span>
+
+            <span
+              style={{
+                color: "red",
+                display: "inline-block",
+                // paddingLeft: "100px",
                 cursor: "pointer",
               }}
               onClick={() => handleDelete(item.id)}
             >
               &#128465;
             </span>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
